e50fe369a0d45907fbab4b6167a9032edec3889c
Freakyab <aryanbhisikar1@gmail.com>
2022-12-21 19:34:26 +0530

    ToDo list

--- src/App.jsx
+++ src/App.jsx
@@ -31,35 +31,95 @@
 //         </>
 //     )
 // }
-import React from "react";
-const json = [
-    {
-        id: 1,
-        text: "i am aryan",
-        des:"nice"
-    },
-    {
-        id: 2,
-        text: "my name is bhisikar",
-        des:"noice"
-    },
-    {
-        id:3,
-        text:"i am king",
-        des:"double noice"
-    }
 
-]
+// import React from "react";
+// const json = [
+//     {
+//         id: 1,
+//         text: "i am aryan",
+//         des:"nice"
+//     },
+//     {
+//         id: 2,
+//         text: "my name is bhisikar",
+//         des:"noice"
+//     },
+//     {
+//         id:3,
+//         text:"i am king",
+//         des:"double noice"
+//     }
 
-const App = () => {
-    // const [Data, setData] = React.useState(json)
+// ]
+
+// const App = () => {
+//     // const [Data, setData] = React.useState(json)
     
-    return (
-        <>
-            {json.map(e => (
-                <span key = {e.id}>{e.text}<br/>{e.des}<br/></span>
-            ))}
+//     return (
+//         <>
+//             {json.map(e => (
+//                 <span key = {e.id}>{e.text}<br/>{e.des}<br/></span>
+//             ))}
         
+//         </>
+//     )
+// }
+import React from "react";
+
+// const App = () => {
+//   const [items, setItems] = React.useState([]);
+
+//   const addItem = (e) => {
+//     e.preventDefault();
+//     setItems([...items, e.target.item.value]);
+//     e.target.item.value = "Enter";
+//   };
+
+//   const removeItem = (index) => {
+//     setItems(items.filter((item, i) => i !== index));
+//   };
+
+//   return (
+//     <>
+//       <form onSubmit={addItem}>
+//         <input name="item" text="Enter"/>
+//         <button>Add</button>
+//       </form>
+//       <ul>
+//         {items.map((item, index) => (
+//           <li key={index}>
+//             {item}
+//             <button onClick={() => removeItem(index)}>X</button>
+//           </li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// };
+const App = () => {
+    const [item,setitem] = React.useState([])
+    const clicked = (e) =>{
+        e.preventDefault()
+        setitem([...item,e.target.item.value]) 
+        e.target.item.value = "";
+        console.log(item)  
+    }
+    const Delete =(index) =>{
+        setitem(item.filter((item,i)=>i!==index))
+    }
+    return(
+        <>
+        <form onSubmit={clicked}>
+            <input type="text" name = "item" />
+            <button>click me</button>
+        </form>
+        <ul>
+        {item.map((e ,index)=>(
+            <li key = {index}>{e}
+            {/* <button onClick={()=>{Delete(index)}}> del</button> */}
+            </li>
+        ))} 
+        </ul>
         </>
     )
 }
